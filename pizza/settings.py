741d755cdc0444097fd5db5700212292f9afe8f3
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2020-04-28 17:47:22 -0300

    changes to settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -126,6 +126,9 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # isso foi adicionado depois
