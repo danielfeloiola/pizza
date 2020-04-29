48a6ec8120cd87c6acebbb4555f87288fa959559
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2020-04-28 22:07:54 -0300

    running locally

--- pizza/settings.py
+++ pizza/settings.py
@@ -142,7 +142,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
