14a3010f1763af671e7a580dd2c5ac08b5294b21
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2020-04-29 15:18:40 -0300

    changed debug settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0:5000','fast-mountain-47414.herokuapp.com/']
 
