6cd6a9b3061f37546c5f45e551069ccf8291d9d1
Daniel Loiola <danf10@me.com>
2026-02-03 15:35:03 -0300

    updated classed in orders file

--- orders/forms.py
+++ orders/forms.py
@@ -1,17 +1,15 @@
 from django import forms
 from .models import RegularPizza, SicilianPizza, Sub, DinnerPlatter, Salad, Pasta, Topping
 
-# Form for the menu
 
 class OrderForm(forms.Form):
-    'form for dinner platters, salads and pasta'
     dish = forms.CharField()
     type = forms.CharField()
     size = forms.CharField()
     price = forms.CharField()
 
+
 class SubForm(forms.Form):
-    "form for subs"
     sub_name = forms.CharField(required=False)
     sub_type = forms.CharField(required=False)
     sub_size = forms.CharField(required=False)
@@ -23,29 +21,23 @@ class SubForm(forms.Form):
 
 
 class PizzaForm(forms.Form):
-    "form for pizzas"
     pizza_dish = forms.CharField()
     pizza_type = forms.CharField()
     pizza_size = forms.CharField()
     pizza_price = forms.CharField()
     num_of_topings = forms.CharField()
 
-    toppings = Topping.objects.all().values()
+    pizza_topping_1 = forms.ChoiceField(required=False)
+    pizza_topping_2 = forms.ChoiceField(required=False)
+    pizza_topping_3 = forms.ChoiceField(required=False)
+    pizza_topping_4 = forms.ChoiceField(required=False)
 
-    final_toppings = []
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    for i in toppings:
-        final_toppings.append((i['name'],i['name']))
+        toppings = Topping.objects.values_list("name", "name")
 
-    pizza_topping_1 = forms.ChoiceField(widget=forms.Select, choices=final_toppings, required=False)
-    pizza_topping_2 = forms.ChoiceField(widget=forms.Select, choices=final_toppings, required=False)
-    pizza_topping_3 = forms.ChoiceField(widget=forms.Select, choices=final_toppings, required=False)
-    pizza_topping_4 = forms.ChoiceField(widget=forms.Select, choices=final_toppings, required=False)
-
-    label = {
-        'pizza_topping_1': None,
-        'pizza_topping_2': None,
-        'pizza_topping_3': None,
-        'pizza_topping_4': None
-
-    }
+        self.fields["pizza_topping_1"].choices = toppings
+        self.fields["pizza_topping_2"].choices = toppings
+        self.fields["pizza_topping_3"].choices = toppings
+        self.fields["pizza_topping_4"].choices = toppings
