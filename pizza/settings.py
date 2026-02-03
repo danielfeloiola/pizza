9e3ed22c2a7acf94248895f4a3d77072c8188fed
Daniel Loiola <danf10@me.com>
2026-02-03 15:54:35 -0300

    removing dotenv

--- pizza/settings.py
+++ pizza/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import dj_database_url
-from dotenv import load_dotenv
 import os
 import psycopg2
 
