f45aaae92dafe756511e24e8f5e9e8684a4d292d
Daniel Loiola <danf10@me.com>
2026-02-03 15:24:57 -0300

    update settings for new dependencies

--- pizza/settings.py
+++ pizza/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 #}
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=0,
     )
 }
