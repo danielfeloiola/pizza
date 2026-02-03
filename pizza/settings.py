dfb1ef1198f1a5aabd13184bcd69fe144d192184
Daniel Loiola <danf10@me.com>
2026-02-03 13:50:48 -0300

    db bugfix

--- pizza/settings.py
+++ pizza/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'PORT': os.getenv('PGPORT', '5432'),
         'OPTIONS': {
             'sslmode': 'require',
-            'options': '-c endpoint=ep-nameless-hat-aizjzql5',
+            'options': '-c endpoint=ep-round-art-aixulurm',
         },
     }
 }
